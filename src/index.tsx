185352047b04dd18c5b051beb5c83283cd480297
André van Delft <andrevandelft@outlook.com>
2024-06-04 10:38:47 +0200

    Future proof presentation selector

--- src/index.tsx
+++ src/index.tsx
@@ -4,28 +4,38 @@ import "./index.css";
 import { Presentation2022 } from "./Presentation2022";
 import { Presentation2024 } from "./Presentation2024";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
+const presentations: Record<string, () => JSX.Element> = {
+  "2022": Presentation2022,
+  "2024": Presentation2024,
+};
 
 const urlParams = new URLSearchParams(window.location.search);
 const year = urlParams.get("year");
 
+function Presentation({ year }: { year: string | null }) {
+  const CurrentPresentation = year && presentations[year];
+
+  if (CurrentPresentation) {
+    return <CurrentPresentation />;
+  }
+
+  return (
+    <ul>
+      {Object.keys(presentations).map((year) => (
+        <li key={year}>
+          <a href={`?year=${year}`}>{year}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+
 root.render(
   <React.StrictMode>
-    {year === "2022" ? (
-      <Presentation2022 />
-    ) : year === "2024" ? (
-      <Presentation2024 />
-    ) : (
-      <ul>
-        <li>
-          <a href="?year=2022">2022</a>
-        </li>
-        <li>
-          <a href="?year=2024">2024</a>
-        </li>
-      </ul>
-    )}
+    <Presentation year={year} />
   </React.StrictMode>,
 );
